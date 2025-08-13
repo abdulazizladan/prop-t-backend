ab2206837414524401dce83894b54c70b428b784
abdulazizladan <abdulazizladan@gmail.com>
2025-08-13 09:33:36 +0100

    task: Make Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configure Swagger documentation
+  const config = new DocumentBuilder()
+    .setTitle('Prop-T documentation') // Set a title for your API documentation
+    .setDescription('Backend services for Prop-T') // Provide a description for the API
+    .setVersion('1.0') // Specify the version of the API
+    .addTag('default') // Add a tag for grouping API endpoints (can be customized)
+    .build();
+
+  // Create the Swagger document based on the application and configuration
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Set up the Swagger UI at a specified endpoint (e.g., http://localhost:3000/api)
+  SwaggerModule.setup('api', app, document);
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
